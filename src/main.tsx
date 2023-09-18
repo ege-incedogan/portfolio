c118ec57b7b89ed8038e55d2b1de08ddbbcb18ef
Ege incedogan <ege1912ksk@gmail.com>
2023-09-18 21:49:14 +0300

    homepage in package, browserrouter to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from "./themes.ts";
 import "./main.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ChakraProvider theme={theme}>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ChakraProvider>
 );
